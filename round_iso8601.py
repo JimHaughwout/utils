0fbbfea88494bf5792870fb994ccf967d0317163
Jim Haughwout <jim.haughwout@gmail.com>
2017-07-08 12:04:18 -0400

    Create round_iso8601.py
    Adds utility to round ISO8601 timestamps to the nearest `N` seconds. Useful as Hive UDF for stitching together time series data of different intervals

---
+++ round_iso8601.py
@@ -0,0 +1,33 @@
+from dateutil.parser import parse
+from datetime import timedelta
+
+
+def round_b(x, base=5):
+    """
+    Rounds a number to base X
+    """
+    return int(base * round(float(x) / base))
+
+def round_iso8601(ts, round_sec=900, iso_format='Z'):
+    """
+    Args: 
+        iso8601 timestamp, rounding_second, iso_format
+    Returns:
+        iso8601 timestamp rounded to the nearest rouding_seconds
+        Formatted with Z(ulu) time to milliseconds if iso_format is 'Z'
+    """
+    dt = parse(ts)
+    secs = dt.microsecond / 1e6 + dt.second + 60. * dt.minute + 3600. * dt.hour 
+    new_secs = round_b(secs, round_sec)
+    
+    dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+    dt = dt + timedelta(seconds=new_secs)
+    
+    ts = dt.isoformat()
+    if iso_format == 'Z':
+        ts = ts.split('+')[0] + '.000Z'
+    return ts
+
+assert round_iso8601('2017-07-07T11:38:20.121Z', 900, 'Z') == '2017-07-07T11:45:00.000Z'
+assert round_iso8601('2017-07-07T11:32:20.121Z', 900, 'Z') == '2017-07-07T11:30:00.000Z'
+print "Tests passed"
